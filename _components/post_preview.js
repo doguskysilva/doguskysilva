b453b5c19339cf7a3b6f9415bc75171dadc10e89
Douglas Silva <dogusky.silva@outlook.com>
2021-02-18 01:23:02 -0300

    adiciona template para o link do post

--- _components/post_preview.js
+++ _components/post_preview.js
@@ -1,16 +1,44 @@
 import Link from "next/link";
+import tinytime from "tinytime"
 
-const PostPreview = (props) => (
-  <div className="mb-4">
-    <h1>
-      <a href={`/posts/${props.post.slug}`}>
-        {props.post.title}  
-      </a>  
-    </h1>
-    <p>{props.post.date}</p>
-    <p>{props.post.excerpt}</p>
-    {/* <Link href={`/posts/${props.post.slug}`}></Link> */}
-  </div>
+
+const postDateTemplate = tinytime('{MM} {DD}, {YYYY}')
+
+const PostPreview = ({ post }) => (
+  <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+    <div className="space-y-3 xl:col-span-4">
+      <div>
+        <h3 className="text-2xl font-bold leading-8 tracking-tight mb-2">
+          <Link href={`/blog/${post.slug}`} className="text-gray-900 dark:text-gray-100">
+            {post.title}
+          </Link>
+        </h3>
+        <div className="flex flex-wrap">
+          <dl className="mr-4">
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+              <time dateTime={post.date}>
+                {postDateTemplate.render(new Date(post.date))}
+              </time>
+            </dd>
+          </dl>
+          {post.tags.map((tag) => (
+            <span className="mr-5 text-lightBlue-600">{tag}</span>
+          ))}
+        </div>
+      </div>
+      <div className="prose text-gray-500 max-w-none dark:text-gray-300">
+        {post.excerpt}
+      </div>
+      <div class="text-base font-medium leading-6 pt-4">
+        <Link href={`/blog/${post.slug}`}>
+          <a class="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400" aria-label={post.title}>
+            Leia mais →
+          </a>
+        </Link>
+      </div>
+    </div>
+  </article>
 )
 
 export default PostPreview
