7baca4484a6b4bba0476dea9e59767ef1b0852f1
Douglas Silva <dogusky.silva@outlook.com>
2021-02-18 21:18:43 -0300

    estrutura para mostrar um post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,6 +2,11 @@ import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import { getAllPosts, getPostBySlug } from '../../lib/api'
 import markdownToHtml from '../../lib/markdownToHtml'
+import tinytime from 'tinytime'
+import Tag from '../../_components/tag'
+import Head from 'next/head'
+
+const postDateTemplate = tinytime('{MM} {DD}, {YYYY}')
 
 function Post({ post, morePost, preview }) {
   const router = useRouter()
@@ -11,11 +16,41 @@ function Post({ post, morePost, preview }) {
 
   return (
     <>
-      <h1>{post.title}</h1>
-      <hr/>
-      {JSON.stringify(post.author)}
-      <hr/>
-      <div dangerouslySetInnerHTML={{ __html: post.content }}/>
+      <Head>
+        <title>{post.title} | dogusky.tech</title>
+      </Head>
+      <section className="p-6 pt-4">
+        <header className="pb-6">
+          <div className="space-y-1 text-center">
+            <div>
+              <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                {post.title}
+              </h1>
+            </div>
+            <dl className="space-y-10 py-4 block">
+              <div>
+                <dt className="sr-only">Published on</dt>
+                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <time datetime={post.date}>
+                    {postDateTemplate.render(new Date(post.date))}
+                  </time>
+                </dd>
+              </div>
+            </dl>
+            <div>
+              <span className="text-center">
+                { post.tags.map(tag => ( <Tag tag={tag} /> )) }
+              </span>
+            </div>
+          </div>
+        </header>
+        <article className="py-8 border-t border-b border-gray-700">
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+        <footer className="block py-6">
+          
+        </footer>
+      </section>
     </>
   )
 }
@@ -26,7 +61,9 @@ export async function getStaticProps({ params }) {
     'title',
     'content',
     'author',
-    'coverImage'
+    'coverImage',
+    'date',
+    'tags'
   ])
   const content = await markdownToHtml(post.content || '')
 
