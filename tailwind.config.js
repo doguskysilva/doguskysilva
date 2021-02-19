80d214e6539dcc2e303b9a3795d66c031bb98629
Douglas Silva <dogusky.silva@outlook.com>
2021-02-18 23:09:16 -0300

    corrige exportacao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    './src/**/*.html',
-     './src/**/*.js',
+    "./pages/**/*.js",
+    "./_components/**/*.js",
   ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
