46cc8679250186cd74b4a3767de38783e02dd739
Douglas Silva <dogusky.silva@outlook.com>
2021-02-22 14:42:39 -0300

    recupera estrutura default do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,6 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
-    colors: {
-      // Build your palette here
-      white: colors.white,
-      transparent: 'transparent',
-      current: 'currentColor',
-      gray: colors.coolGray,
-      red: colors.red,
-      lightBlue: colors.lightBlue,
-      blue: colors.blue,
-      cyan: colors.cyan,
-      blueGray: colors.blueGray,
-      yellow: colors.amber,
-    }
   },
   variants: {
     extend: {},
