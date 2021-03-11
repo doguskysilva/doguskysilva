4cff100af57b5a808cab9ca3e9f5a5cad944fcb3
Douglas Silva <dogusky.silva@outlook.com>
2021-03-11 15:21:37 -0300

    Adiciona conteudo da página de index

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,27 @@ const Index = () => (
       <title>Douglas Silva | dogusky.tech</title>
     </Head>
     <div className='h-full flex flex-wrap content-center px-6 py-3 lg:px-10 lg:py-5 main-background'>
-      <div className='md:w-2/3'>
-        <h1 className='text-6xl'>Douglas Silva</h1>
-        <p className='text-xl my-8 text-left'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut doloribus, exercitationem laborum impedit nulla sapiente inventore, perferendis eos quia asperiores nam.
-        </p>
-        <button>
-          Leia Mais
-        </button>
+      <div className='w-full'>
+        <div className='md:w-2/3'>
+          <h1 className='text-6xl'>{'<Douglas Silva />'}</h1>
+          <p className='text-xl my-8 text-left'>
+            Desenvolvedor de software, apaixonado por programação e agora escrevendo, compartilhando tudo que aprendi e continuo aprendendo
+          </p>
+        </div>
+        
+        <div className='flex w-full'>
+          <div className='flex-1 bg-violet-900'>
+            Ola Mundo
+          </div>
+          <div className='flex-1 bg-violet-700'>
+            Ola Mundo
+          </div>
+        </div>
       </div>
     </div>
   </Layout>
 )
+
+
 
 export default Index
