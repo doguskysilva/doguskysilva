76572fd80bb1dfd5fd3d7c30701caec0b9067456
Douglas Silva <dogusky.silva@outlook.com>
2023-09-20 11:37:02 -0300

    feat: something

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,14 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
+    '@nuxt/devtools',
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    locales: ["en", "pt"],
+    strategy: "prefix_except_default",
+    defaultLocale: "en",
+    detectBrowserLanguage: false,
+    vueI18n: './i18n.config.ts'
+  }
 })
